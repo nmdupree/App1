8b0822798b04e5f9990b594351142b75d2725efe
Nicole Dupree <nicoledupree88@gmail.com>
2019-11-01 11:37:07 -0400

    Added PostgresDB, Hikari connection pool, and first REST endpoint (DashboardController, DashboardService)

---
+++ webapp/src/main/java/com/lessons/services/DashboardService.java
@@ -0,0 +1,49 @@
+package com.lessons.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@Service
+public class DashboardService
+{
+    private static final Logger logger = LoggerFactory.getLogger(DashboardService.class);
+
+    @Resource
+    private DataSource dataSource;             // Injected data source
+
+
+    public DashboardService()
+    {
+        logger.debug("DashboardService() Constructor called");
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        logger.debug("DashboardService post constructor called.");
+    }
+
+
+    public String getDatabaseTime()
+    {
+        logger.debug("getDatabaseTime() started.");
+
+        // Construct a SQL select query to get the current date time
+        String sql = "Select NOW()";
+
+        JdbcTemplate jt = new JdbcTemplate(this.dataSource);
+
+        // Get a connection from the connection pool, run the query, return the value, return the connection to the connection pool
+        String sDateTime = jt.queryForObject(sql, String.class);
+
+        // This method returns date/time as a string
+        logger.debug("Database Time is {}", sDateTime);
+        return sDateTime;
+    }
+}
